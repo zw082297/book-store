b843b1557425f8d9a52a8a35526fd7432b748fca
zengwei <zw740435366@163.com>
2023-05-28 14:44:08 +0800

    1.测试001

--- src/main/java/com/book/bookstore/BookStoreApplication.java
+++ src/main/java/com/book/bookstore/BookStoreApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookStoreApplication {
-
+    /**
+     * 123
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
